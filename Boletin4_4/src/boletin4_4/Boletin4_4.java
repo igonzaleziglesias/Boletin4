597c17ba5ddb5ef869dd01e02c9f3251d05127e1
igonzaleziglesias <igonzaleziglesias@nautilus5.danielcastelao.org>
2018-10-23 09:16:32 +0200

    modificacion ejercicio 4 del boletin 4

--- Boletin4_4/src/boletin4_4/Boletin4_4.java
+++ Boletin4_4/src/boletin4_4/Boletin4_4.java
@@ -4,46 +4,52 @@ import java.util.Scanner;
 
 public class Boletin4_4 {
 
-   
-    public static void main(String[] args) {
-        Scanner ler = new Scanner(System.in);
-        Polbo restaurante = new Polbo(2,3);
-        Boolean r;
-        
-        restaurante.amosarPolbo();
-        restaurante.amosarPatatas();
-        
-        System.out.print("Pueden comer: "+ restaurante.clientes()+" clientes");   
-        System.out.println("\nComen?(true/false): ");
-        r = ler.nextBoolean();
-        if (r==true){
-            restaurante.comen();
-        } 
-        restaurante.amosarPolbo();
-        restaurante.amosarPatatas();
+    
+        public static void main(String[] args) {
+                Scanner ler = new Scanner(System.in);
+                Polbo restaurante = new Polbo(2,3);
+                Boolean r;
+                Boolean s=false;
         
         
-        System.out.print("\nCuantos kg de Polbo engade?: ");
-        restaurante.engadirPolbo(ler.nextInt());
-        System.out.print("Cuantos kg de Patatas engade?: ");
-        restaurante.engadirPatatas(ler.nextInt());
+                while (s==false){
+                        restaurante.amosarPolbo();
+                        restaurante.amosarPatatas();
         
         
-        System.out.print("\nPueden comer: "+ restaurante.clientes()+" clientes");
-        System.out.print("\nComen?(true/false): ");
-        r = ler.nextBoolean();
-        if (r=true){
-            restaurante.comen();
-        } 
-        
-        restaurante.amosarPolbo();
-        restaurante.amosarPatatas();
-        
-        
-        
+                        System.out.print("Pueden comer: "+ restaurante.clientes()
+                        +" clientes");   
+                        System.out.println("\nComen?(true/false): ");
+                        r = ler.nextBoolean();
+                        
+                        if (r==true){
+                                restaurante.comen();
+                        } 
+                        restaurante.amosarPolbo();
+                        restaurante.amosarPatatas();
         
         
+                        System.out.print("\nCuantos kg de Polbo engade?: ");
+                        restaurante.engadirPolbo(ler.nextInt());
+                        System.out.print("Cuantos kg de Patatas engade?: ");
+                        restaurante.engadirPatatas(ler.nextInt());
         
-    }   
+                        System.out.println("Quiere salir?(true/false): ");
+                        s=ler.nextBoolean();
+                }
+        
+                        /*System.out.print("\nPueden comer: "
+                        + restaurante.clientes()+" clientes");
+                        System.out.print("\nComen?(true/false): ");
+                        r = ler.nextBoolean();
+                        if (r=true){
+                                restaurante.comen();
+                        } 
+        
+                        restaurante.amosarPolbo();
+                        restaurante.amosarPatatas();
+                        */
+
+                }   
     
 }
